ce35616b672327079b48ac6c0968842fe99b4371
SeranDzh <sjaparov87@gmail.com>
2026-02-12 16:04:46 +0000

    adding a js file to the product card layout

---
+++ script.js
@@ -0,0 +1,39 @@
+const switchFirstCardButton = document.getElementById('switch-first-card-button');
+const firstProductCard = document.querySelector('.product-card');
+const blueHashColor = "#2200ff";
+
+switchFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueHashColor;
+})
+
+const productCards = document.querySelectorAll('.product-card');
+const switchAllCardButton = document.getElementById('switch-all-card-button');
+const yellowHashColor = 'hsl(66, 100%, 50%)';
+
+switchAllCardButton.addEventListener('click', () => {
+  productCards.forEach(
+    card => card.style.backgroundColor = yellowHashColor
+  )
+})
+
+const googleOpenButton = document.getElementById('open-google-button');
+googleOpenButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите перейти на сайт Google?');
+
+  if (answer === true) {
+    window.open('https://google.com');
+  } else { 
+}}
+
+const colorToggleSwitchButton =document.querySelector('#color-toggle-switch-button');
+colorToggleSwitchButton.addEventListener('click', () => {
+  colorToggleSwitchButton.classList.toggle('active');
+})
+
+const mainTitle = document.querySelector('.main-title');
+mainTitle.addEventListener('mouseover', () => {
+  console.log('я выбрал продукт');
+})
+
